f63c3111c174ee61104654cc85c3df6c4e01be5e
Demokdawa <Demokdawa@yahoo.fr>
2019-09-27 14:29:47 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,11 +14,11 @@ async def on_ready():
     
 @bot.event
 async def on_message(message):
-	if message.author == bot.user:
+    if message.author == bot.user:
         return
-	if message.content == "?":
+    if message.content == "?":
         await message.channel.send("Ouais ouais ouais !")
-	await bot.process_commands(message)
+    await bot.process_commands(message)
 	
     
 bot.run("NjI3MTEwMzM1ODAyNzY5NDA4.XY34wA.ksGsiEaAlgzbZlYVldLSrjivmKM")
