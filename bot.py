19d42224556e699711ca07f99928ddfced099186
Demokdawa <Demokdawa@yahoo.fr>
2019-10-08 17:00:24 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,28 +16,24 @@ print('[Init] Bot configuré !')
 async def on_ready():
     print("[Init] Bot en ligne !")
     await bot.change_presence(activity=discord.Game("lel"))
-    
-    
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content == "sendmeme":
-        subreddit = reddit.subreddit("dankmemes")
-        imageUrls = []
-        for submission in subreddit.hot(limit=400):
-            if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
-                imageUrls.append(submission.url)
 
-        randomImage = imageUrls[random.randint(0,len(imageUrls) - 1)]
-        req.urlretrieve(randomImage, 'tempDiscord.jpg')
-        fullPath = os.path.join(os.getcwd(), 'tempDiscord.jpg') 
-    
-        file = discord.File(fullPath)
-        await message.channel.send(file=file)
 
-        os.remove('tempDiscord.jpg')
-    await bot.process_commands(message)
-	
+@bot.command()
+async def sendmeme(ctx)
+    subreddit = reddit.subreddit("dankmemes")
+    imageUrls = []
+    for submission in subreddit.hot(limit=400):
+        if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
+            imageUrls.append(submission.url)
+
+    randomImage = imageUrls[random.randint(0,len(imageUrls) - 1)]
+    req.urlretrieve(randomImage, 'tempDiscord.jpg')
+    fullPath = os.path.join(os.getcwd(), 'tempDiscord.jpg') 
+
+    file = discord.File(fullPath)
+    await message.channel.send(file=file)
+
+    os.remove('tempDiscord.jpg')
+
    
 bot.run("NjI3MTEwMzM1ODAyNzY5NDA4.XY34wA.ksGsiEaAlgzbZlYVldLSrjivmKM")
