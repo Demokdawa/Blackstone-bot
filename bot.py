cb36de9d780913e8cba55f645e9860956520b307
Demokdawa <Demokdawa@yahoo.fr>
2019-10-08 17:18:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,6 +34,23 @@ async def sendmeme(ctx):
     await ctx.channel.send(file=file)
 
     os.remove('tempDiscord.jpg')
+    
+@bot.command()
+async def sendlewdmeme(ctx):
+    subreddit = reddit.subreddit("hentaidankmemes")
+    imageUrls = []
+    for submission in subreddit.hot(limit=400):
+        if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
+            imageUrls.append(submission.url)
+
+    randomImage = imageUrls[random.randint(0,len(imageUrls) - 1)]
+    req.urlretrieve(randomImage, 'tempDiscord.jpg')
+    fullPath = os.path.join(os.getcwd(), 'tempDiscord.jpg') 
+
+    file = discord.File(fullPath)
+    await ctx.channel.send(file=file)
+
+    os.remove('tempDiscord.jpg')
 
    
 bot.run("NjI3MTEwMzM1ODAyNzY5NDA4.XY34wA.ksGsiEaAlgzbZlYVldLSrjivmKM")
