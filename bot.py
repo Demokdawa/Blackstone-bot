1f332905e1fdd54029306243156e0004f144e651
Demokdawa <Demokdawa@yahoo.fr>
2019-10-09 16:30:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,6 +72,42 @@ async def sendwfmeme(ctx):
     await ctx.channel.send(file=file)
 
     os.remove('tempDiscord.jpg')
+    
+   
+@bot.command()
+async def sendcursed(ctx):
+    subreddit = reddit.subreddit("cursedimages")
+    image_urls = []
+    for submission in subreddit.top(limit=100):
+        if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
+            image_urls.append(submission.url)
 
+    random_image = image_urls[random.randint(0,len(image_urls) - 1)]
+    req.urlretrieve(random_image, 'tempDiscord.jpg')
+    full_path = os.path.join(os.getcwd(), 'tempDiscord.jpg')
+
+    file = discord.File(full_path)
+    await ctx.channel.send(file=file)
+
+    os.remove('tempDiscord.jpg')
+
+
+@bot.command()
+async def sendyum(ctx):
+    subreddit = reddit.subreddit(" FoodPorn")
+    image_urls = []
+    for submission in subreddit.top(limit=100):
+        if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
+            image_urls.append(submission.url)
+
+    random_image = image_urls[random.randint(0,len(image_urls) - 1)]
+    req.urlretrieve(random_image, 'tempDiscord.jpg')
+    full_path = os.path.join(os.getcwd(), 'tempDiscord.jpg')
+
+    file = discord.File(full_path)
+    await ctx.channel.send(file=file)
+
+    os.remove('tempDiscord.jpg')
+   
    
 bot.run("NjI3MTEwMzM1ODAyNzY5NDA4.XY34wA.ksGsiEaAlgzbZlYVldLSrjivmKM")
