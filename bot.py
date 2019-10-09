d3f572e37f6c1009cb1276c9c12b3ae521de9eaf
Demokdawa <Demokdawa@yahoo.fr>
2019-10-09 17:32:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def sendmeme(ctx):
 
     file = discord.File(full_path)
     await ctx.channel.send(file=file)
-    await ctx.message.delete(ctx)
+    await ctx.message.delete(message)
     os.remove('tempDiscord.jpg')
     
 
