154968680876df302cb4e5f932b869f1f01cb3b0
Demokdawa <Demokdawa@yahoo.fr>
2019-10-09 16:55:05 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,6 @@ async def sendmeme(ctx):
     subreddit = reddit.subreddit("dankmemes")
     image_urls = []
     for submission in subreddit.top(limit=100):
-        print("--- %s seconds ---" % (time.time() - start_time))
         if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
             image_urls.append(submission.url)
 
@@ -108,6 +107,67 @@ async def sendyum(ctx):
     await ctx.channel.send(file=file)
 
     os.remove('tempDiscord.jpg')
+  
+  
+@bot.command()
+async def sendearth(ctx):
+    subreddit = reddit.subreddit("EarthPorn")
+    image_urls = []
+    for submission in subreddit.top(limit=100):
+        if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
+            image_urls.append(submission.url)
+
+    random_image = image_urls[random.randint(0,len(image_urls) - 1)]
+    req.urlretrieve(random_image, 'tempDiscord.jpg')
+    full_path = os.path.join(os.getcwd(), 'tempDiscord.jpg')
+
+    file = discord.File(full_path)
+    await ctx.channel.send(file=file)
+
+    os.remove('tempDiscord.jpg')
+    
+ 
+@bot.command()
+async def sendnocontext(ctx):
+    subreddit = reddit.subreddit("nocontextpics")
+    image_urls = []
+    for submission in subreddit.top(limit=100):
+        if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
+            image_urls.append(submission.url)
+
+    random_image = image_urls[random.randint(0,len(image_urls) - 1)]
+    req.urlretrieve(random_image, 'tempDiscord.jpg')
+    full_path = os.path.join(os.getcwd(), 'tempDiscord.jpg')
+
+    file = discord.File(full_path)
+    await ctx.channel.send(file=file)
+
+    os.remove('tempDiscord.jpg')
    
+   
+@bot.command()
+async def sendwtf(ctx):
+    subreddit = reddit.subreddit("WTF")
+    image_urls = []
+    for submission in subreddit.top(limit=100):
+        if submission.url.endswith('.jpg') or submission.url.endswith('.png'):
+            image_urls.append(submission.url)
+
+    random_image = image_urls[random.randint(0,len(image_urls) - 1)]
+    req.urlretrieve(random_image, 'tempDiscord.jpg')
+    full_path = os.path.join(os.getcwd(), 'tempDiscord.jpg')
+
+    file = discord.File(full_path)
+    await ctx.channel.send(file=file)
+
+    os.remove('tempDiscord.jpg')
+
+
+@bot.command(pass_context=True)
+async def deletethis(ctx, user: discord.Member):
+    await bot.say('Command received')
+    await bot.delete_message(ctx.message)
+    await bot.say('Message deleted')
+
    
 bot.run("NjI3MTEwMzM1ODAyNzY5NDA4.XY34wA.ksGsiEaAlgzbZlYVldLSrjivmKM")
