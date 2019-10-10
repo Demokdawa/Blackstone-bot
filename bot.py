1bc9e50f96f9588c76d9ecf0dad6bdd1dbbed2bf
Demokdawa <Demokdawa@yahoo.fr>
2019-10-10 12:09:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -186,7 +186,7 @@ async def sendaww(ctx):
 @bot.command(pass_context=True)
 async def react(ctx):
     msg = await ctx.channel.send("Done")
-    await msg.add_reaction('GreenTick:375576212568801280')
+    await msg.add_reaction(':greenTick:596576670815879169')
 
 
 bot.run("NjI3MTEwMzM1ODAyNzY5NDA4.XY34wA.ksGsiEaAlgzbZlYVldLSrjivmKM")
