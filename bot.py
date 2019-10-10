b34d563f949f45784a4cd0174371ea56e4be127b
Demokdawa <Demokdawa@yahoo.fr>
2019-10-10 14:50:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -193,7 +193,7 @@ async def react(ctx):
     try:
         reaction, user = await bot.wait_for('reaction_add', timeout=6.0, check=check)
     except asyncio.TimeoutError:
-        await ctx.channel.delete()
+        await ctx.message.delete()
     else:
         await ctx.channel.send('good !')
     
