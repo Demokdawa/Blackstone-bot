205e4f782eb9f2496bd754feb8b09daec819fbd7
Demokdawa <Demokdawa@yahoo.fr>
2019-10-11 09:58:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ async def sendmeme(ctx):
     await img.add_reaction('\N{WHITE HEAVY CHECK MARK}')
     await img.add_reaction('\N{CROSS MARK}')
 
-    def check(reaction, user):
+    def check(reaction):
         return str(reaction.emoji) in ['\N{WHITE HEAVY CHECK MARK}', '\N{CROSS MARK}']
     try:
         reaction = await bot.wait_for('reaction_add', timeout=14.0, check=check)
