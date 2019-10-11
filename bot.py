e0ed2dbbf6abec8ac6d2764ddc72367d0fa94eda
Demokdawa <Demokdawa@yahoo.fr>
2019-10-11 15:44:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,16 +46,15 @@ async def sendmeme(ctx):
     await img.add_reaction('\N{WHITE HEAVY CHECK MARK}')
     await img.add_reaction('\N{CROSS MARK}')
 
-    def check(reaction, user):               
-        return user.bot is False and str(reaction.emoji) in ['\N{WHITE HEAVY CHECK MARK}', '\N{CROSS MARK}']
+    def check(reaction, user):
+        return user.bot is False and str(reaction.emoji) in ['\N{WHITE HEAVY CHECK MARK}', '\N{CROSS MARK}'] and reaction.message.id == img.id
     try:
-        reaction, user = await bot.wait_for('reaction_add', timeout=14.0, check=check)                                                                                                                                
-        await img.delete()
-        await ctx.message.delete()
+        reaction, user = await bot.wait_for('reaction_add', timeout=14.0, check=check)
     except asyncio.TimeoutError:
         await img.delete()
         await ctx.message.delete()
     else:
+        
         if str(reaction.emoji) == '\N{WHITE HEAVY CHECK MARK}':
             await img.clear_reactions()
         else:
