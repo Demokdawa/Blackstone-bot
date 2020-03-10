c4e4f9bcddadb9e88b12bd041439704196a48922
PRIETO Antoine <antoine.prieto@unither-pharma.com>
2020-03-10 18:09:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -987,7 +987,7 @@ async def sup(ctx):
 # !warn [pseudo] command to warn a specific user
 @bot.command()
 async def warn(ctx, a1):
-    user = ctx.message.mentions[0].member.id
+    user = ctx.message.mentions[0].id
     if user:
         cursor = db.cursor()
         # TestQuery
