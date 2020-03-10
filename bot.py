168dc9c064f6565c580330c322dc04669968d173
PRIETO Antoine <antoine.prieto@unither-pharma.com>
2020-03-10 17:25:02 +0100

    update

--- bot.py
+++ bot.py
@@ -987,8 +987,18 @@ async def sup(ctx):
 # !warn [pseudo] command to warn a specific user
 @bot.command()
 async def warn(ctx):
+    cursor = db.cursor()
+    # TestQuery
+    cursor.execute('''SELECT IDUser FROM Users WHERE DiscordUserId = ?''', (ctx.message.author.id,))
+    result = cursor.fetchone()
+    if result:
+        cursor.execute('''UPDATE Users SET WarnsNumber = WarnsNumber + 1 WHERE DiscordUserId = ?''', (ctx.message.author.id,))
+    else:
+        cursor.execute('''INSERT INTO Users (DiscordUserId, DiscordUserTag, WarnsNumber) VALUES (?,?,?)''', (ctx.message.author.id, ctx.message.author, 1))
     
-    await ctx.channel.send(ctx.message.author.id)
+    db.commit()
+    
+    await ctx.channel.send(str(ctx.message.author.id) + ' is warned !')
 
 
 # !halp command for help
