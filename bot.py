5bcd386666e3d93bf36adc0ab1e3b83a90569c1c
PRIETO Antoine <antoine.prieto@unither-pharma.com>
2020-03-10 18:17:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -990,12 +990,12 @@ async def warn(ctx, a1):
     mentionned_id = ctx.message.mentions[0].id
     cursor = db.cursor()
     # TestQuery
-    cursor.execute('''SELECT IDUser FROM Users WHERE DiscordUserId = ?''', (mentionned_id,))
+    cursor.execute('''SELECT IDUser FROM Users WHERE DiscordUserId = ?''', (ctx.message.mentions[0].id,))
     result = cursor.fetchone()
     if result:
-        cursor.execute('''UPDATE Users SET WarnsNumber = WarnsNumber + 1 WHERE DiscordUserId = ?''', (mentionned_id,))
+        cursor.execute('''UPDATE Users SET WarnsNumber = WarnsNumber + 1 WHERE DiscordUserId = ?''', (ctx.message.mentions[0].id,))
     else:
-        cursor.execute('''INSERT INTO Users (DiscordUserId, DiscordUserTag, WarnsNumber) VALUES (?,?,?)''', (mentionned_id, str(ctx.message.author), 1))
+        cursor.execute('''INSERT INTO Users (DiscordUserId, DiscordUserTag, WarnsNumber) VALUES (?,?,?)''', (ctx.message.mentions[0].id, str(ctx.message.author), 1))
     db.commit()
     print(ctx.message.mentions[0])
     await ctx.channel.send(str(mentionned_id) + ' is warned !')
